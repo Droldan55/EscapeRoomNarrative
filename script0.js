6b6e7bb5e9a527f659e738b3abadb2376e6978ba
Droldan55 <89206199+Droldan55@users.noreply.github.com>
2021-11-04 21:54:32 -0400

    Add files via upload

---
+++ script0.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+  x.style.height = "230px";
+  x.style.width = "230px";
+}
+
+function normalImg(x) {
+  x.style.height = "200px";
+  x.style.width = "200px";
+}
---
+++ script1.js
@@ -0,0 +1,3 @@
+//Audio still doesn't want to play. 
+window.onload = function() {
+    document.getElementById("my_Audio").play();
